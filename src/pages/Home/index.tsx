045b59bbbd291d6cf99fac975624cef3eb771b10
CiceroRMG <cicero.ufsc@gmail.com>
2024-11-25 17:30:41 -0300

    fix: arruma bug de criar um novo ciclo quando ja tinha outro rodando

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -55,18 +55,25 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
     reset()
   }
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -78,6 +85,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const second = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${second}`
+    }
+  }, [minutes, second, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
