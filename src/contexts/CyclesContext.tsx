86398ac64927852ab5e11f74f00f0b31896c0011
CiceroRMG <cicero.ufsc@gmail.com>
2024-11-26 13:35:02 -0300

    refactor: separa logica de contexto para um arquivo novo e usa esse novo Context para todas as rotas.

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,105 @@
+import { createContext, ReactNode, useState } from 'react'
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface createCycleData {
+  task: string
+  minutesAmount: number
+}
+
+interface CyclesContextProviderProps {
+  children: ReactNode
+}
+
+interface CyclesContextType {
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  cycles: Cycle[]
+  insertFinishedDateOnActiveCycle: () => void
+  setSecondsPassed: (seconds: number) => void
+  interruptedCurrentCycle: () => void
+  createNewCycle: (data: createCycleData) => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextType)
+
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  function createNewCycle(data: createCycleData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      minutesAmount: data.minutesAmount,
+      task: data.task,
+      startDate: new Date(),
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+    // reset()
+  }
+
+  function interruptedCurrentCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+
+    setActiveCycleId(null)
+  }
+
+  function insertFinishedDateOnActiveCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+  }
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        activeCycle,
+        activeCycleId,
+        amountSecondsPassed,
+        cycles,
+        insertFinishedDateOnActiveCycle,
+        setSecondsPassed,
+        interruptedCurrentCycle,
+        createNewCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
+}
